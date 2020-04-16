1de665fcf73e28b180cc1b56ce98b0aba1b0603d
WerasutK <zawowwowaz68@gmail.com>
2020-04-16 20:11:34 +0700

    Update regis.php
    Insert validation in html code pattern and insert address

--- app/regis.php
+++ app/regis.php
@@ -28,19 +28,19 @@
         <form action="regis.php" method="POST">
             <div class="form-group">
                 <label for="firstname">First name :</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" required>
+                <input type="text" class="form-control" id="firstname" name="firstname" minlength='2' pattern="[A-Za-z]{2,}|[ก-๙]{2,}" title="Must be Thai / Eng language and contain at least 2 characters" required>
             </div>
             <div class="form-group">
                 <label for="lastname">Lastname :</label>
-                <input type="text" class="form-control" id="lastname" name="lastname" required>
+                <input type="text" class="form-control" id="lastname" name="lastname" minlength='2' pattern="[A-Za-z]{2,}|[ก-๙]{2,}" title="Must be Thai / Eng language and contain at least 2 characters" required>
             </div>
             <div class="form-group">
                 <label for="username">Username :</label>
-                <input type="text" class="form-control" id="username" name="username" required>
+                <input type="text" class="form-control" id="username" name="username" pattern="[A-Za-z0-9]{1,}" required>
             </div>
             <div class="form-group">
                 <label for="password">Password :</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <input type="password" class="form-control" id="password" name="password" pattern=".{6,}" title="Must contain at least 6 characters" required>
             </div>
             <!-- <div class="form-group">
                 <label for="repeat-password">Repeat-Password :</label>
@@ -51,13 +51,17 @@
                 <input type="email" class="form-control" id="email" name="email">
             </div>
             <div class="form-group">
+                <label for="address">Address :</label>
+                <input type="text" class="form-control" id="address" name="address">
+            </div>
+            <div class="form-group">
                 <label for="phone1">Phone Number :</label>
-                <input type="tel" class="form-control" id="phone1" name="phone1" pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+                <input type="tel" class="form-control" id="phone1" name="phone1" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" title="Only 10 numbers"
                     required>
             </div>
             <div class="form-group">
                 <label for="phone2">Phone Number (Optional) :</label>
-                <input type="tel" class="form-control" id="phone2" name="phone2" pattern="[0-9]{3}[0-9]{3}[0-9]{4}">
+                <input type="tel" class="form-control" id="phone2" name="phone2" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" title="Only 10 numbers">
             </div>
             <button type="submit" class="btn btn-primary" name="register">Register</button>
             <br><br>
@@ -76,14 +80,19 @@
             $email = $_POST['email'];
             $phone1 = $_POST['phone1'];
             $phone2 = $_POST['phone2'];
+            $address = $_POST['address'];
             $type = 'customer';
 
             // Create Connection
             $conn = new mysqli("34.87.109.220", "werasutk", "password", "db");
+            $conn1 = new mysqli("34.87.109.220", "werasutk", "password", "db");
 
             // Check Connection
             if ($conn->connect_error) {
                 die("Connection Failed: " . $conn->connect_error);
+            }
+            if ($conn1->connect_error) {
+                die("Connection Failed: " . $conn1->connect_error);
             }
 
             // Check username
@@ -99,17 +108,23 @@
             // Insert Data
             $sql = "INSERT INTO user (username, `password`, firstname, lastname, email, phone1, phone2, `type`)
                     VALUES ('$username', '$password', '$firstname', '$lastname', '$email', '$phone1', '$phone2', '$type')";
+            $sql1 = "INSERT INTO customer (user_username, `address`)
+                     VALUES ('$username', '$address')";
 
-            if ($conn->query($sql) === TRUE) {
+            if (($conn->query($sql) === TRUE) && ($conn1->query($sql1) ===TRUE )) {
                 echo '<script language="javascript">';
                 echo 'alert("You registered success!")';
                 echo '</script>';
+            } elseif ($conn->query($sql) === TRUE) {
+                echo ("Error: " . $sql1 . "<br>" . $conn1->error);
             } else {
                 echo ("Error: " . $sql . "<br>" . $conn->error);
+                echo ("Error: " . $sql1 . "<br>" . $conn1->error);
             }
 
             // Close Connection
             $conn->close();
+            $conn1->close();
         }
 
     ?>
