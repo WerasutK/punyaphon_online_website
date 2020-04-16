a4d61fb0937e20842098b0b169c9850b6b962af7
massakorn <42903869+massakorn@users.noreply.github.com>
2020-04-17 00:59:40 +0700

    Update Session

--- app/home.php
+++ app/home.php
@@ -1,3 +1,8 @@
+<?php
+
+    session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +12,7 @@
 </head>
 <body>
     <h1>Home page!</h1>
+    <h3>สวัสดีคุณ <?php echo $_SESSION['name']; ?></h3>
     <a href="logout.php" class="btn btn-info" role="button">Log out</a>
 </body>
 </html>
