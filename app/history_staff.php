6ea185c18368a9826863fd83bad96a83a641a49e
Massakorn <61070357@kmitl.ac.th>
2020-04-19 23:39:59 +0700

    history for staff (not finissh)

---
+++ app/history_staff.php
@@ -0,0 +1,58 @@
+<?php
+
+    //session_start();
+
+    //if (!isset($_SESSION['username'])) {
+    //    header('Location: login.php');
+    //}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Prompt', sans-serif;
+            text-align: center;
+        }
+        img {
+            width: 100px;
+        }
+
+        </style>
+</head>
+<body>
+<?php
+    // Create Connection
+    $conn = new mysqli("34.87.109.220", "werasutk", "password", "db");
+
+    // Check Connection
+    if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM `history` LEFT OUTER JOIN `payment` ON `history`.`history_id` = `payment`.`payment_id`";
+    $result = $conn->query($sql);
+    echo '<table style="width:100%">';
+    echo '<tr>';
+    echo '<th>History ID</th>';
+    echo '<th>Total Price</th>';
+    echo '</tr>';
+    while($row = $result->fetch_assoc()) { ?>
+        <tr>
+        <td><?php echo $row['history_id']; ?></td>
+        <td><?php echo $row['total_price']; ?></td>
+        </tr>
+    <?php } 
+        // Close Connection
+        $conn->close();
+    ?>
+</body>
+</html>
