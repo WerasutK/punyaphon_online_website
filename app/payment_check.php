f5a2295326c227a00163c7c211f47307439f1203
WerasutK <zawowwowaz68@gmail.com>
2020-04-22 17:35:20 +0700

    Validate Role
    Validate role of customer and staff to have permission to access orther page

--- app/payment_check.php
+++ app/payment_check.php
@@ -2,9 +2,14 @@
 
     session_start();
 
-    //if (!isset($_SESSION['username'])) {
-    //    header('Location: login.php');
-    //}
+    if ((!isset($_SESSION['username'])) || $_SESSION['type'] != 'staff') {
+        session_destroy();
+        // header('Location: login.php');
+        echo "<script language='javascript'>;
+                    alert('Permission Denied!');
+                    window.location='login.php';
+            </script>";
+    }
 
 ?>
 <!DOCTYPE html>
